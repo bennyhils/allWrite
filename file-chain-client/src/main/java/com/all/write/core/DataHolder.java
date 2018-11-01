dc755fd070fc7c99e709125fca65b0ad393c647e
aleksandresin <alexander.esin@gmail.com>
2018-11-01 17:49:39 +0700

    Blocks in holder

--- file-chain-client/src/main/java/com/all/write/core/DataHolder.java
+++ file-chain-client/src/main/java/com/all/write/core/DataHolder.java
@@ -2,14 +2,17 @@ package com.all.write.core;
 
 
 import com.all.write.NetworkMember;
+import com.all.write.api.Block;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @Component
 public class DataHolder {
     private List<NetworkMember> networkMembers = new ArrayList<NetworkMember>();
+    private List<Block> blocks = new LinkedList<Block>();
 
     public List<NetworkMember> getAllNetworkMembers() {
         return networkMembers;
@@ -19,5 +22,12 @@ public class DataHolder {
         this.networkMembers = networkMembers;
     }
 
+    public List<Block> getBlocks() {
+        return blocks;
+    }
+
+    public void setBlocks(List<Block> blocks) {
+        this.blocks = blocks;
+    }
 
 }
