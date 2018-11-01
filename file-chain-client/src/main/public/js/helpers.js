89a8bf14ba8b9bfebbf1463e7cee40c0ee790747
Karo <karo@webkaro.ru>
2018-11-01 21:43:17 +0300

    werwerew

--- file-chain-client/src/main/public/js/helpers.js
+++
@@ -1,8 +0,0 @@
-function findUserByAddress(address, users) {
-    for (let i = 0; i < users.length; ++i) {
-        if (address == users[i].address) {
-            return users[i];
-        }
-    }
-    return 0;
-}
